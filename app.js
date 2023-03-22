09e597301618e6d1fc1f5be665c55c1e157cdec5
Lautaro Demonte <lauty.d.p@gmail.com>
2023-03-22 11:16:30 -0300

    agrego la vista del home con todos los productos

--- app.js
+++ app.js
@@ -1,24 +1,38 @@
 import express from "express";
 import routers from "./src/routers/index.routers.js";
-
-//import { dirname } from "path";
-//import { fileURLToPath } from "url";
+import { __dirname } from "./src/utils.js";
+import handlebars from "express-handlebars";
+import ProductManager from "./src/controllers/productManager.js";
 
 const app = express();
 const PORT = 8080;
-//const __dirname = dirname(fileURLToPath(import.meta.url));
+const productManager = new ProductManager("src/db/products.json");
 
+/* middlewares */
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(__dirname + "/public"));
 
-app.get("/", (req, res) => {
-  try {
-    res.send(
-      `<h1 style="text-align: center">Bienvenido a mi nueva entrega! 😎</h1>`
-    );
-  } catch (error) {
-    console.log(`Error cargando el proyecto: ${error.message}`);
-  }
+/* handlebars */
+app.engine(
+  "hbs",
+  handlebars.engine({
+    extname: "hbs",
+    defaultLayout: "main.hbs",
+    layoutsDir: __dirname + "/views/layouts",
+  })
+);
+app.set("views", __dirname + "/views");
+app.set("view engine", "hbs");
+
+app.get("/", async (req, res) => {
+  const products = await productManager.getAll();
+  console.log(products);
+  res.render("home", {
+    style: "home.css",
+    title: "Home",
+    products: products,
+  });
 });
 
 app.use("/api", routers);
